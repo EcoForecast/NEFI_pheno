d7f34955af74ab59aad6865e510c083f31e58294
Kathryn Wheeler <kiwheel@bu.edu>
2017-10-05 14:06:19 -0400

    Added to the script to have it take the desired NEON products from the googlesheet and condensed the for loops.

--- check_NEON_data_products.R
+++ check_NEON_data_products.R
@@ -3,39 +3,42 @@
 #install.packages(c("ggplot2", "devtools", "dplyr", "stringr"))
 #install.packages(c("maps", "mapdata"))
 #install.packages("ggmap")
+#install.packages("googlesheets")
 
 library(ggplot2)
 library(ggmap)
 library(maps)
 library(mapdata)
 library(nneo)
+library(googlesheets)
 
+neon_google_sheet <- gs_url('https://docs.google.com/spreadsheets/d/1IhnQR6wvMhbjYBu_furIWTXKtnduFNH-pVslC45zU2E/edit#gid=1224285869',lookup=TRUE)
+gs_download(from=neon_google_sheet,to='NEON_Products.csv',overwrite=TRUE,ws='NEON Products')
 pdf("NEON_Site_Product_Availability.pdf", width=6, height=4)
 neon_sites <- nneo_sites()
 desiredProducts <- read.csv(file="NEON_Products.csv",head=TRUE)
 
 dta <-  map_data("world",c("usa","hawaii","alaska"),xlim=c(-180,-65), ylim=c(19,72))
 
-for (k in 1:(dim(desiredProducts)[1])){
+for (k in seq_len(nrow(desiredProducts))){
 
 gg1 <- ggplot(main=desiredProducts$NEON.Product[k]) + geom_polygon(data = dta, aes(x=long, y=lat, group=group),fill=NA,colour="black") + coord_fixed(1.3)
-gg1 <- gg1 + geom_point(data=neon_sites, aes(x = siteLongitude, y = siteLatitude),colour="red",show.legend = TRUE) 
+#gg1 <- gg1 + geom_point(data=neon_sites, aes(x = siteLongitude, y = siteLatitude),colour="red",show.legend = TRUE) 
 
 productName <-  desiredProducts$NEON.Product[k] 
 productID <- paste(substring(desiredProducts$NEON.Product.ID[k],6,14),".001",sep="") #properly format the product ID's
 neon_sites$containsProduct <- NA
 neon_sites$productLength <- NA
-for (i in 1:(dim(neon_sites)[1])){
+for (i in seq_len(nrow(neon_sites))){
   availableProducts = neon_sites[i,]$dataProducts[[1]]$dataProductCode
   availableDates = list()
   containsProductBool <-  FALSE
-  if(length(availableProducts>0)){
-  for (j in 1:length(availableProducts)){
+  for(j in seq_along(availableProducts)){
+    if(!is.na(availableProducts[j])){ #sometimes neon includes random NA's apparently
     if (availableProducts[j]== productID){
       containsProductBool <-  TRUE
       availableDates <- c(availableDates,neon_sites[i,]$dataProducts[[1]]$availableMonths[j])
-    }
-  }
+    }}
   }
   if(length(availableDates)>0){
     neon_sites$productLength[i] <- length(availableDates[[1]])
