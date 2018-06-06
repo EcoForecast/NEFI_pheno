7e89051c4ac60b6180eeb3cb8af444e0f58cf3af
kiwheel@bu.edu <kiwheel@geo.bu.edu>
2018-06-06 14:47:23 -0400

    updated the createNDVI_GOES file

--- PhenologyBayesModeling/R/createNDVI_GOES.R
+++ PhenologyBayesModeling/R/createNDVI_GOES.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+
 library("ncdf4")
 library(plyr)
 
@@ -13,8 +14,9 @@ createNDVI_GOES <- function(lat,long,siteID){
 
   NDVI.vals <- list()
 
-  days <- c(seq(109,126),132,seq(134,136),seq(139,143),seq(146,154),seq(156,157),seq(159,161),seq(163,181),seq(182,188),seq(191,200),seq(203,207),209,seq(211,214),216,seq(218,220),seq(222,231),seq(233,264),seq(266,269),seq(271,289),seq(291,295),seq(297,301),seq(303,314),seq(316,324),seq(328,333),348,seq(350,365))
-  for (i in days){
+  days1 <- c(seq(109,126),132,seq(134,136),seq(139,143),seq(146,154),seq(156,157),seq(159,161),seq(163,181),seq(182,188),seq(191,200),seq(203,207),209,seq(211,214),216,seq(218,220),seq(222,231),seq(233,264),seq(266,269),seq(271,289),seq(291,295),seq(297,301),seq(303,314),seq(316,324),seq(328,333)
+
+  for (i in days1){
     if(i<100){
       day.time <- paste("20170",i,"165",sep="")
     }
@@ -37,23 +39,62 @@ createNDVI_GOES <- function(lat,long,siteID){
     }
 
   }
+  days2 <- c(348,seq(350,365))
+  for (i in days2){
+    if(i<100){
+      day.time <- paste("20170",i,"165",sep="")
+    }
+    else{
+      day.time <- paste("2017",i,"165",sep="")
+    }
 
-  fileName <- paste("GOES_NDVI_",siteID,"2017_kappaDQF.csv",sep="")
+
+    filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s",day.time,sep="")
+    ACM.file <-nc_open(paste("GOES_Data2017/",dir(path="GOES_Data2017",pattern=filestrACM),sep=""))
+
+    print(i)
+    clouds <- ncvar_get(ACM.file,"BCM")[ACM.ind[1],ACM.ind[2]]
+    if(clouds == 0){
+      NDVI.val <- getSpecificNDVI(Ind2,Ind3,day.time)
+      NDVI.vals <- c(NDVI.vals,NDVI.val)
+    }
+    else{
+      NDVI.vals <- c(NDVI.vals,NA)
+    }
+
+  }
+  days3 <- seq(1,60)
+
+  for (i in days3){
+    if(i<10) {
+      day.time <- paste("201800",i,"165",sep="")
+    else if(i<100){
+      day.time <- paste("20180",i,"165",sep="")
+    }
+    else{
+      day.time <- paste("2018",i,"165",sep="")
+    }
+
+
+    filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s",day.time,sep="")
+    ACM.file <-nc_open(paste("GOES_Data2017/",dir(path="GOES_Data2017",pattern=filestrACM),sep=""))
+
+    print(i)
+    clouds <- ncvar_get(ACM.file,"BCM")[ACM.ind[1],ACM.ind[2]]
+    if(clouds == 0){
+      NDVI.val <- getSpecificNDVI(Ind2,Ind3,day.time)
+      NDVI.vals <- c(NDVI.vals,NDVI.val)
+    }
+    else{
+      NDVI.vals <- c(NDVI.vals,NA)
+    }
+
+  }
+
+  fileName <- paste("GOES_NDVI_",siteID,"_kappaDQF.csv",sep="")
   output <- rbind(t(days),NDVI.vals)
   write.table(output,file=fileName,sep=",",col.names=FALSE,row.names=FALSE)
 }
-
-# siteData <- read.csv("GOES_Paper_Sites.csv",header=FALSE)
-#
-# desiredNo <- c(6,8,16)
-#
-# for (i in desiredNo){
-#   siteName <- siteData[i,1]
-#   lat <- siteData[i,2]
-#   long <-siteData[i,3]
-#   print(siteName)
-#   mainGraphing(lat,long,siteName)
-# }
 
 #days <- c(seq(182,207),209,seq(211,214),seq(216,220),seq(222,231),seq(233,264),seq(266,269),seq(271,289),seq(291,295),seq(297,301),seq(303,314),seq(316,324),seq(328,333),348,seq(350,365))
 #days <- c(seq(182,207),209,seq(211,214),seq(216,220),seq(222,231),seq(233,264),seq(266,269),seq(271,295),seq(297,301),seq(303,324),seq(328,333),seq(348,365))
