278e47d88085f262402d9b632c9a03bd61c08bfe
Kathryn Wheeler <kiwheel@bu.edu>
2018-06-20 17:59:04 -0400

    Added a needleleaf evergreen Bayes model

--- mainComparingPhenoRS.R
+++ mainComparingPhenoRS.R
@@ -7,10 +7,10 @@ library("runjags")
 
 siteData <- read.csv("GOES_Paper_Sites.csv",header=FALSE)
 startDay <- 110
-endDay <- 424
+endDay <- 474
 xseq <- seq(startDay,endDay,1)
 
-iseq <- seq(1,19)
+iseq <- seq(1,5)
 
 for(i in iseq){
   siteName <- as.character(siteData[i,1])
@@ -31,14 +31,12 @@ for(i in iseq){
       MODIS.md.out <- runMCMC_Model(j.model=j.model.MODIS,variableNames = DB.vars)
       save(var.burn,file=fileName)
     }
-    
     fileName <- paste(siteName,"_GOES_varBurn.RData",sep="")
     if(!file.exists(fileName)){
       j.model.GOES <- createBayesModel.DB(dataSource="GOES.NDVI",siteName=siteName)
       GOES.md.out <- runMCMC_Model(j.model=j.model.GOES,variableNames = DB.vars)
       save(var.burn,file=fileName)
     }
-  
   }
   else if(PFT=="SH"){
     SH.vars <- c("a","b","c","d","k","r","prec")
@@ -54,7 +52,6 @@ for(i in iseq){
       MODIS.md.out <- runMCMC_Model(j.model=j.model.MODIS,variableNames = SH.vars)
       save(var.burn,file=fileName)
     }
-    
     fileName <- paste(siteName,"_GOES_varBurn.RData",sep="")
     if(!file.exists(fileName)){
       j.model.GOES <- createBayesModel.SH(dataSource="GOES.NDVI",siteName=siteName)
@@ -62,13 +59,32 @@ for(i in iseq){
       save(var.burn,file=fileName)
     }
   }
-  #graphMCMC_Outputs(outputFileName = "SH_PhenoFit.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=7,startDay = startDay,endDay = endDay)
-    
+  else if(PFT=="EN"){
+    EN.vars <- c("a","b","c","d","prec")
+    fileName <- paste(siteName,"_PC_varBurn.RData",sep="")
+    if(!file.exists(fileName)){
+      j.model.PC <- createBayesModel.SH(dataSource="PC.GCC",siteName=siteName,URL=URL)
+      PC.md.out <- runMCMC_Model(j.model=j.model.PC,variableNames = EN.vars)
+      save(var.burn,file=fileName)
+    }
+    fileName <- paste(siteName,"_MODIS_varBurn.RData",sep="")
+    if(!file.exists(fileName)){
+      j.model.MODIS <- createBayesModel.SH(dataSource="MODIS.NDVI",siteName=siteName)
+      MODIS.md.out <- runMCMC_Model(j.model=j.model.MODIS,variableNames = EN.vars)
+      save(var.burn,file=fileName)
+    }
+    fileName <- paste(siteName,"_GOES_varBurn.RData",sep="")
+    if(!file.exists(fileName)){
+      j.model.GOES <- createBayesModel.SH(dataSource="GOES.NDVI",siteName=siteName)
+      GOES.md.out <- runMCMC_Model(j.model=j.model.GOES,variableNames = EN.vars)
+      save(var.burn,file=fileName)
+    }
+  }
 
 }
   
 #graphMCMC_Outputs(outputFileName = "BayesFits_SH3.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,18,19),startDay=110,endDay=424)
-graphMCMC_Outputs_withData(outputFileName = "BayesFits_SH2.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,18,19),startDay=110,endDay=424)
-
+#graphMCMC_Outputs_withData(outputFileName = "BayesFits_SH3.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,15),startDay=110,endDay=474)
+#createOutputSummary(siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,15),startDay=110,endDay=474)
 
 
