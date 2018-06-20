9c066141eb3719c6aafba91915083aa589a555fb
kiwheel@bu.edu <kiwheel@geo.bu.edu>
2018-06-20 18:02:59 -0400

    Still trying to get everything up to date

--- mainComparingPhenoRS.R
+++ mainComparingPhenoRS.R
@@ -7,10 +7,11 @@ library("runjags")
 
 siteData <- read.csv("GOES_Paper_Sites.csv",header=FALSE)
 startDay <- 110
-endDay <- 424
+endDay <- 474
 xseq <- seq(startDay,endDay,1)
 
-iseq <- seq(1,19)
+#iseq <- seq(1,19)
+iseq <- c(18)
 
 for(i in iseq){
   siteName <- as.character(siteData[i,1])
@@ -19,56 +20,55 @@ for(i in iseq){
   PFT <- as.character(siteData[i,5])
   if(PFT=="DB"){
     DB.vars <- c("a","b","c","d","prec")
-    fileName <- paste(siteName,"_PC_varBurn.RData",sep="")
+    fileName <- paste(siteName,"_PC_varBurnINITS.RData",sep="")
     if(!file.exists(fileName)){
       j.model.PC <- createBayesModel.DB(dataSource="PC.GCC",siteName=siteName,URL=URL)
       PC.md.out <- runMCMC_Model(j.model=j.model.PC,variableNames = DB.vars)
-      save(var.burn,file=fileName)
+      save(PC.md.out,file=fileName)
     }
-    fileName <- paste(siteName,"_MODIS_varBurn.RData",sep="")
+    fileName <- paste(siteName,"_MODIS_varBurnINITS.RData",sep="")
     if(!file.exists(fileName)){
       j.model.MODIS <- createBayesModel.DB(dataSource="MODIS.NDVI",siteName=siteName)
       MODIS.md.out <- runMCMC_Model(j.model=j.model.MODIS,variableNames = DB.vars)
-      save(var.burn,file=fileName)
+      save(MODIS.md.out,file=fileName)
     }
     
-    fileName <- paste(siteName,"_GOES_varBurn.RData",sep="")
+    fileName <- paste(siteName,"_GOES_varBurnINITS.RData",sep="")
     if(!file.exists(fileName)){
       j.model.GOES <- createBayesModel.DB(dataSource="GOES.NDVI",siteName=siteName)
       GOES.md.out <- runMCMC_Model(j.model=j.model.GOES,variableNames = DB.vars)
-      save(var.burn,file=fileName)
+      save(GOES.md.out,file=fileName)
     }
   
   }
   else if(PFT=="SH"){
     SH.vars <- c("a","b","c","d","k","r","prec")
-    fileName <- paste(siteName,"_PC_varBurn.RData",sep="")
+    fileName <- paste(siteName,"_PC_varBurnINITS.RData",sep="")
     if(!file.exists(fileName)){
       j.model.PC <- createBayesModel.SH(dataSource="PC.GCC",siteName=siteName,URL=URL)
-      PC.md.out <- runMCMC_Model(j.model=j.model.PC,variableNames = SH.vars)
-      save(var.burn,file=fileName)
+      PC.md.out <- runMCMC_Model(j.model=j.model.PC,variableNames = SH.vars,maxIter=10**6)
+      save(PC.md.out,file=fileName)
     }
-    fileName <- paste(siteName,"_MODIS_varBurn.RData",sep="")
+    fileName <- paste(siteName,"_MODIS_varBurnINITS.RData",sep="")
     if(!file.exists(fileName)){
       j.model.MODIS <- createBayesModel.SH(dataSource="MODIS.NDVI",siteName=siteName)
-      MODIS.md.out <- runMCMC_Model(j.model=j.model.MODIS,variableNames = SH.vars)
-      save(var.burn,file=fileName)
+      MODIS.md.out <- runMCMC_Model(j.model=j.model.MODIS,variableNames = SH.vars,maxIter=10**6)
+      save(MODIS.md.out,file=fileName)
     }
     
-    fileName <- paste(siteName,"_GOES_varBurn.RData",sep="")
+    fileName <- paste(siteName,"_GOES_varBurnINITS.RData",sep="")
     if(!file.exists(fileName)){
       j.model.GOES <- createBayesModel.SH(dataSource="GOES.NDVI",siteName=siteName)
-      GOES.md.out <- runMCMC_Model(j.model=j.model.GOES,variableNames = SH.vars)
-      save(var.burn,file=fileName)
+      GOES.md.out <- runMCMC_Model(j.model=j.model.GOES,variableNames = SH.vars,maxIter=10**6)
+      save(GOES.md.out,file=fileName)
     }
   }
   #graphMCMC_Outputs(outputFileName = "SH_PhenoFit.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=7,startDay = startDay,endDay = endDay)
     
-
 }
   
 #graphMCMC_Outputs(outputFileName = "BayesFits_SH3.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,18,19),startDay=110,endDay=424)
-graphMCMC_Outputs_withData(outputFileName = "BayesFits_SH2.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,18,19),startDay=110,endDay=424)
+#graphMCMC_Outputs_withData(outputFileName = "BayesFits_SH2.pdf",siteFileName = "GOES_Paper_Sites.csv",iseq=c(7,18,19),startDay=110,endDay=424)
 
 
 
