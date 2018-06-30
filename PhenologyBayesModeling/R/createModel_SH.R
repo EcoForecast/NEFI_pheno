ebc6229501644c2a4b76293c057a1cf945d6ca90
Kathryn Wheeler <kiwheel@bu.edu>
2018-06-30 13:36:44 -0400

    Added missing code to create the data and inits variables for MODIS EVI for shrubland

--- PhenologyBayesModeling/R/createModel_SH.R
+++ PhenologyBayesModeling/R/createModel_SH.R
@@ -36,6 +36,15 @@ createBayesModel.SH <- function(dataSource,siteName="",URL="",startDay,endDay,la
     data$mean.c <- 0.25
     data$mean.d <- 0.15
   }
+  else if(dataSource == "MODIS.EVI"){
+    data = MODIS_data(siteName=siteName,lat=lat,long=long,startDay = startDay,endDay = endDay,metric="EVI")
+    inits.mu <- createInits_SH(data)
+    for(i in 1:(nchain)){
+      inits[[i]] <- list(Tran=rnorm(1,150,10),b=rnorm(1,0.11,0.05),c=rnorm(1,inits.mu$c,0.02),d=rnorm(1,inits.mu$d,0.02),r=rnorm(1,-0.02,0.002),k=rnorm(1,inits.mu$k,5))
+    }
+    data$mean.c <- 0.25
+    data$mean.d <- 0.15
+  }
   else if(dataSource=="GOES.NDVI"){
     data = GOES_data(siteName,startDay = 110,endDay = 424)
     inits.mu <- createInits_SH(data)
