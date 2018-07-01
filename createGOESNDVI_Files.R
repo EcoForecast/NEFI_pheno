78aed776e4221ff022f4270415823767f305f908
kiwheel@bu.edu <kiwheel@geo.bu.edu>
2018-07-01 13:56:51 -0400

    Trying to merge createGOESNDVI_Files

--- createGOESNDVI_Files.R
+++ createGOESNDVI_Files.R
@@ -1,15 +1,18 @@
 #!/usr/bin/env Rscript
 
+install.packages("PhenologyBayesModeling",repo=NULL)
 library("ncdf4")
 library(plyr)
 library("PhenologyBayesModeling")
 
 siteData <- read.csv("GOES_Paper_Sites.csv",header=FALSE)
 startDay <- 110
-endDay <- 424
+endDay <- 474
 
-#iseq <- c(7,15,18,19,20,21)
-iseq <- c(20,21)
+iseq <- c(9,14,17)
+#iseq <- c(1,2,3)
+#iseq <- c(4,5,6)
+#iseq <- c(8,10,12,13)
 for(i in iseq){
   siteName <- as.character(siteData[i,1])
   print(siteName)
