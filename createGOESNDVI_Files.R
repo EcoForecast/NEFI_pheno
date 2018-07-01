d247c8b67d3f098dc9d4cfb52576721e743fc56b
kiwheel@bu.edu <kiwheel@geo.bu.edu>
2018-07-01 13:57:58 -0400

    Merged

--- createGOESNDVI_Files.R
+++ createGOESNDVI_Files.R
@@ -26,23 +26,3 @@ for(i in 1:nrow(siteData)){
   GOES_data <- function(siteName,lat,long,startDay,endDay,TZ)
 }
 
-
-
-
-
-siteData <- read.csv("GOES_Paper_Sites.csv",header=FALSE)
-startDay <- 110
-endDay <- 474
-
-iseq <- c(9,14,17)
-#iseq <- c(1,2,3)
-#iseq <- c(4,5,6)
-#iseq <- c(8,10,12,13)
-for(i in iseq){
-  siteName <- as.character(siteData[i,1])
-  print(siteName)
-  lat <- as.numeric(siteData[i,2])
-  long <- as.numeric(siteData[i,3])
-  #print(c(lat,long))
-  createNDVI_GOES(lat=lat,long=long,siteID=siteName)
-}
