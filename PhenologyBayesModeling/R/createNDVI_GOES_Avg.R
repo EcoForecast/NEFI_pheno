abdc98c269e3dd41a1a387cd62b36137da19dcad
Kathryn Wheeler <kiwheel@bu.edu>
2018-07-11 19:31:50 -0400

    Updated to calculate full NDVI average

--- PhenologyBayesModeling/R/createNDVI_GOES_Avg.R
+++ PhenologyBayesModeling/R/createNDVI_GOES_Avg.R
@@ -25,8 +25,7 @@ createNDVI_GOES_Avg <- function(lat,long,startDay,endDay,fileName,TZ){
   NDVI.vals <- numeric()
   NDVI.vars <- numeric()
   days <- numeric()
-  #days1 <- seq(startDay,333)
-  days1 <- seq(startDay,180)
+  days1 <- seq(startDay,333)
 
   for (i in days1){
     NDVI.day.vals <- numeric()
@@ -65,72 +64,94 @@ createNDVI_GOES_Avg <- function(lat,long,startDay,endDay,fileName,TZ){
     NDVI.vals <- c(NDVI.vals,mean(NDVI.day.vals,na.rm = TRUE))
     NDVI.vars <- c(NDVI.vars,var(NDVI.day.vals,na.rm = TRUE))
     days <- c(days,i)
-    }
+  }
 
-  # Ind2 <- getDataIndex(getABI_Index(lat.rd,long.rd,orbitVersion="NEW"),2,orbitVersion="NEW")
-  # Ind3 <- getDataIndex(getABI_Index(lat.rd,long.rd,orbitVersion="NEW"),3,orbitVersion="NEW")
-  # ACM.ind <- getDataIndex(getABI_Index(lat.rd,long.rd,orbitVersion="NEW"),"ACM",orbitVersion="NEW")
-  #
-  # days2 <- seq(348,365,1)
-  # for (i in days2){
-  #   day.time <- paste("2017",i,Tstr,sep="")
-  #
-  #   filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s",day.time,sep="")
-  #   filepath <- paste("GOES_Data2017/",dir(path="GOES_Data2017",pattern=filestrACM),sep="")
-  #   if(file.exists(filepath) && !dir.exists(filepath)){
-  #     ACM.file <-nc_open(filepath)
-  #
-  #     print(day.time)
-  #     clouds <- ncvar_get(ACM.file,"BCM")[ACM.ind[1],ACM.ind[2]]
-  #     if(!is.na(clouds)){
-  #       if(clouds == 0){
-  #         NDVI.val <- getSpecificNDVI(Ind2,Ind3,day.time)
-  #         NDVI.vals <- c(NDVI.vals,NDVI.val)
-  #       }
-  #       else{
-  #         NDVI.vals <- c(NDVI.vals,NA)
-  #       }
-  #     }
-  #     else{
-  #       NDVI.vals <- c(NDVI.vals,NA)
-  #     }
-  #     days <- c(days,i)
-  #   }
-  # }
-  # days3 <- c(seq(1,(endDay-365),1))
-  #
-  # for (i in days3){
-  #   print(i)
-  #   if(i<10) {
-  #     day.time <- paste("201800",i,Tstr,sep="")
-  #   }
-  #   else if(i<100){
-  #     day.time <- paste("20180",i,Tstr,sep="")
-  #   }
-  #   else{
-  #     day.time <- paste("2018",i,Tstr,sep="")
-  #   }
-  #
-  #   filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s",day.time,sep="")
-  #   filepath <- paste("GOES_Data2017/",dir(path="GOES_Data2017",pattern=filestrACM),sep="")
-  #   if(file.exists(filepath) && !dir.exists(filepath)){
-  #     ACM.file <-nc_open(filepath)
-  #     clouds <- ncvar_get(ACM.file,"BCM")[ACM.ind[1],ACM.ind[2]]
-  #     if(!is.na(clouds)){
-  #       if(clouds == 0){
-  #         NDVI.val <- getSpecificNDVI(Ind2,Ind3,day.time)
-  #         NDVI.vals <- c(NDVI.vals,NDVI.val)
-  #       }
-  #       else{
-  #         NDVI.vals <- c(NDVI.vals,NA)
-  #       }
-  #     }
-  #     else{
-  #       NDVI.vals <- c(NDVI.vals,NA)
-  #     }
-  #     days <- c(days,i)
-  #   }
-  # }
+   Ind2 <- getDataIndex(getABI_Index(lat.rd,long.rd,orbitVersion="NEW"),2,orbitVersion="NEW")
+   Ind3 <- getDataIndex(getABI_Index(lat.rd,long.rd,orbitVersion="NEW"),3,orbitVersion="NEW")
+   ACM.ind <- getDataIndex(getABI_Index(lat.rd,long.rd,orbitVersion="NEW"),"ACM",orbitVersion="NEW")
+
+   days2 <- seq(348,365,1)
+
+   for (i in days2){
+     NDVI.day.vals <- numeric()
+     if(i<100){
+       day <- paste("20170",i,sep="")
+     }
+     else{
+       day <- paste("2017",i,sep="")
+     }
+     for(j in seq(startT,endT,1)){
+       day.hr <- paste(as.character(day),j,sep="")
+       print(day.hr)
+       filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s",day.hr,sep="")
+       ACMfiles <- dir(path="GOES_Data2017",pattern=filestrACM)
+       print(length(ACMfiles))
+       for(f in ACMfiles){
+         day.time <- substr(f,24,34)
+         print(day.time)
+         ACM.file <-nc_open(paste("GOES_Data2017/",f,sep=""))
+         clouds <- ncvar_get(ACM.file,"BCM")[ACM.ind[1],ACM.ind[2]]
+         if(!is.na(clouds)){
+           if(clouds == 0){
+             NDVI.val <- getSpecificNDVI(Ind2,Ind3,day.time)
+             #print(NDVI.val)
+             NDVI.day.vals <- c(NDVI.day.vals,NDVI.val)
+           }
+           else{
+             NDVI.day.vals <- c(NDVI.day.vals,NA)
+           }
+         }
+         else{
+           NDVI.day.vals <- c(NDVI.day.vals,NA)
+         }
+       }
+     }
+     NDVI.vals <- c(NDVI.vals,mean(NDVI.day.vals,na.rm = TRUE))
+     NDVI.vars <- c(NDVI.vars,var(NDVI.day.vals,na.rm = TRUE))
+     days <- c(days,i)
+   }
+
+   days3 <- c(seq(1,(endDay-365),1))
+
+   for (i in days3){
+     NDVI.day.vals <- numeric()
+     if(i<100){
+       day <- paste("20170",i,sep="")
+     }
+     else{
+       day <- paste("2017",i,sep="")
+     }
+     for(j in seq(startT,endT,1)){
+       day.hr <- paste(as.character(day),j,sep="")
+       print(day.hr)
+       filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s",day.hr,sep="")
+       ACMfiles <- dir(path="GOES_Data2017",pattern=filestrACM)
+       print(length(ACMfiles))
+       for(f in ACMfiles){
+         day.time <- substr(f,24,34)
+         print(day.time)
+         ACM.file <-nc_open(paste("GOES_Data2017/",f,sep=""))
+         clouds <- ncvar_get(ACM.file,"BCM")[ACM.ind[1],ACM.ind[2]]
+         if(!is.na(clouds)){
+           if(clouds == 0){
+             NDVI.val <- getSpecificNDVI(Ind2,Ind3,day.time)
+             #print(NDVI.val)
+             NDVI.day.vals <- c(NDVI.day.vals,NDVI.val)
+           }
+           else{
+             NDVI.day.vals <- c(NDVI.day.vals,NA)
+           }
+         }
+         else{
+           NDVI.day.vals <- c(NDVI.day.vals,NA)
+         }
+       }
+     }
+     NDVI.vals <- c(NDVI.vals,mean(NDVI.day.vals,na.rm = TRUE))
+     NDVI.vars <- c(NDVI.vars,var(NDVI.day.vals,na.rm = TRUE))
+     days <- c(days,i)
+   }
+
   output <- rbind(t(days),NDVI.vals,NDVI.vars)
   write.table(output,file=fileName,sep=",",col.names=FALSE,row.names=FALSE)
 }
