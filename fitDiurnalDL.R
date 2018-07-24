da085deb0d0aac97ddcc7eaa1d75bec5471cdb60
Kathryn Wheeler <kiwheel@bu.edu>
2018-07-24 16:10:03 -0400

    Updated to add beta prior

--- fitDiurnalDL.R
+++ fitDiurnalDL.R
@@ -10,7 +10,8 @@ library("MODISTools")
 
 
 
-dat <- read.csv("GOES_NDVI_missouriozarks_20170606.csv",header=FALSE)
+#dat <- read.csv("GOES_NDVI_missouriozarks_20170606.csv",header=FALSE)
+dat <- read.csv("GOES_Diurnal_HarvardForest_2017July2.csv",header=FALSE)
 data <- list()
 data$x <- as.numeric(substr(dat[1,],8,9))+as.numeric(substr(dat[1,],10,11))/60
 for(i in 1:length(data$x)){
@@ -26,14 +27,16 @@ data$y <- as.numeric(dat[2,])
 #lines(x <- xseq,y <- pheno.logistic(Tran=24,b=1.8,d=0.3,c=0.48,xseq=xseq),col="red")
 nchain = 5
 inits <- list()
-
+#,d=rnorm(1,0.3,0.1)
 for(i in 1:nchain){
-  inits[[i]] <- list(TranL=rnorm(1,11.95,0.1),bL=rnorm(1,-1.5,0.3),TranR=rnorm(1,24,0.1),bR=rnorm(1,1.8,0.2),c=rnorm(1,0.48,0.5),d=rnorm(1,0.3,0.1),k=rnorm(1,17.5,1))
+  inits[[i]] <- list(TranL=rnorm(1,11.95,0.1),bL=rnorm(1,-1.5,0.3),TranR=rnorm(1,24,0.1),bR=rnorm(1,1.8,0.2),c=rnorm(1,0.8,0.05),k=rnorm(1,17.5,1))
 }
-data$mean.c <- 0.48
-data$mean.d <- 0.3
-data$p.c <- 1/(0.5**2)
-data$p.d <- 1/(0.5**2)
+#data$mean.c <- 0.48
+#data$mean.d <- 0.3
+#data$p.c <- 1/(0.5**2)
+#data$p.d <- 1/(0.5**2)
+data$alpha.c <- 11
+data$beta.c <- 3
 
 data$s1 <- 0.001
 data$s2 <- 0.00001
@@ -54,14 +57,15 @@ TranL ~ dnorm(mean.TranL,p.Tran) ##S for spring
 bL ~ dnorm(mean.bL,p.b)
 TranR ~ dnorm(mean.TranR,p.Tran)  ##F for fall/autumn
 bR ~ dnorm(mean.bR,p.b)
-d ~ dnorm(mean.d,p.d)
-c ~ dnorm(mean.c,p.c)
+#d ~ dnorm(mean.d,p.d)
+#c ~ dnorm(mean.c,p.c)
+c ~ dbeta(alpha.c,beta.c)
 k ~ dnorm(mean.k,p.k)
 prec ~ dgamma(s1,s2)
 
 for(i in 1:n){
-muL[i] <- c/(1+exp(bL*(x[i]-TranL)))+d ##process model for fall
-muR[i] <- c/(1+exp(bR*(x[i]-TranR)))+d ##process model for Spring
+muL[i] <- c/(1+exp(bL*(x[i]-TranL))) ##process model for left
+muR[i] <- c/(1+exp(bR*(x[i]-TranR))) ##process model for right
 mu[i] <- ifelse(x[i]>k,muR[i],muL[i])   #change point process model
 
 y[i]  ~ dnorm(mu[i],prec)		## data model
@@ -72,5 +76,5 @@ j.model   <- jags.model(file = textConnection(DB_model_DL),
                         data = data,
                         inits=inits,
                         n.chains = nchain)
-md.out <- runMCMC_Model(j.model,variableNames=c("TranL","bL","TranR","bR","c","d","k"))
-save(md.out,file="missouriozarks_20170606_DL_varBurn.RData")
+md.out <- runMCMC_Model(j.model,variableNames=c("TranL","bL","TranR","bR","c","k"))
+save(md.out,file="HarvardForest_2017July2_DL_varBurn.RData")
