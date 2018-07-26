a1f446fd2d01a33f2f6e92dbee24654434857221
Kathryn Wheeler <kiwheel@bu.edu>
2018-07-26 16:22:32 -0400

    Still trying to fix error

--- createAllDiurnalFilesYear2018_2.R
+++ createAllDiurnalFilesYear2018_2.R
@@ -44,14 +44,6 @@ createNDVI_GOES_diurnal <- function(lat,long,siteID,startDay,endDay,orbitVersion
     }
   }
   for (i in 1:length(days)){
-    #print(days)
-    days[i] <- as.numeric(days[i])
-    if(days[i] < 10){
-     days[i] <- paste("00",as.character(days[i]),sep="")
-    }
-    else if(days[i] < 100){
-     days[i] <- paste("0",as.character(days[i]),sep="")
-    }
     print(days[i])
     #days[i] <- as.character(days[i])
     filestrACM <- paste("OR_ABI-L2-ACMC-M3_G16_s2018",days[i],sep="")
