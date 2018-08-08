0ca3c3131619a40e3fa0679527aadd23c5e5791b
kiwheel@bu.edu <kiwheel@scc-gb08.scc.bu.edu>
2018-08-08 16:29:30 -0400

    Updated diurnalFits

--- createAllDiurnalFits.R
+++ createAllDiurnalFits.R
@@ -11,7 +11,7 @@ library("doParallel")
 
 #detect cores.
 n.cores <- detectCores()
-n.cores <- 2
+n.cores <- 4
 
 #register the cores.
 registerDoParallel(cores=n.cores)
@@ -19,7 +19,11 @@ registerDoParallel(cores=n.cores)
 siteName <- "russellSage"
 #diurnal.files <- dir(path="dailyNDVI_GOES",pattern=paste("GOES_Diurnal_",siteName,sep=""))
 #iseq <- c(186,191,198,230,248,250,252,285)
-iseq <- c(186,191,198,230)
+#iseq <- c(seq(186,193),seq(195,201),206,207,211,217,230,231,seq(233,236),seq(244,254),258,259,seq(277,287),seq(297,299),seq(301,304),seq(313,315))
+#iseq <- c(seq(182,321,1),seq(348,364,1))
+iseq <- c(seq(182,193),seq(195,203),seq(206,207),seq(211,213),seq(215,217),224,seq(227,231),seq(233,236),seq(244,250),seq(251,254),seq(258,260),seq(262,268),seq(271,274),seq(277,291),seq(296,299),seq(301,304),seq(306,309),seq(313,315),seq(318,320),321,355,363)
+
+
 #i=191
 
 output <- foreach(i = iseq) %dopar% {
@@ -28,24 +32,24 @@ output <- foreach(i = iseq) %dopar% {
   fileName <- paste("dailyNDVI_GOES/","GOES_Diurnal_",siteName,"_2017",i,".csv",sep="")
   print(fileName)
   dat <- read.csv(fileName,header=FALSE)
-  data.v <- list()
+  data <- list()
   print(dim(dat))
   data$x <- as.numeric(dat[3,])
   data$y <- as.numeric(dat[2,])
   #plot(data$x,data$y)
-  j.model <- createBayesModel.Diurnal(siteName=siteName,data)
+  outFileName <- paste(siteName,"_",as.character(i),"_varBurn2.RData",sep="")
+  if(!file.exists(outFileName)){
+    j.model <- createBayesModel.Diurnal(siteName=siteName,data)
   
-  var.burn <- runMCMC_Model(j.model = j.model,variableNames=c("a","c","k","prec"),iterSize = 50000)#,baseNum = 1000000,iterSize = 70000)
-  counter <- 1
-  while(counter < 5){
-    if(typeof(var.burn)==typeof(FALSE)){
-      j.model <- createBayesModel.Diurnal(siteName=siteName,data)
-      var.burn <- runMCMC_Model(j.model = j.model,variableNames=c("c","prec"))
+    var.burn <- runMCMC_Model(j.model = j.model,variableNames=c("a","c","k","prec"),iterSize = 50000)#,baseNum = 1000000,iterSize = 70000)
+    counter <- 1
+    while(counter < 5){
+      if(typeof(var.burn)==typeof(FALSE)){
+        j.model <- createBayesModel.Diurnal(siteName=siteName,data)
+        var.burn <- runMCMC_Model(j.model = j.model,variableNames=c("a","c","k","prec"))
+      }
+      counter <- counter + 1
     }
-    counter <- counter + 1
-  }
-  if(typeof(var.burn)!=typeof(FALSE)){
-    outFileName <- paste(siteName,"_",as.character(i),"_varBurn.RData",sep="")
     save(var.burn,file=outFileName)
   }
 }
