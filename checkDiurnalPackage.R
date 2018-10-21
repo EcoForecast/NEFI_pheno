a82715e6a29809dcd17fd7114e4b277c98b30b58
Kathryn Wheeler <kiwheel@bu.edu>
2018-10-21 19:45:24 -0400

    testing more

--- checkDiurnalPackage.R
+++ checkDiurnalPackage.R
@@ -6,9 +6,9 @@ install.packages("/projectnb/dietzelab/kiwheel/NEFI_pheno/GOESDiurnalNDVI",repo=
 library("GOESDiurnalNDVI")
 print("Checking calNDVI()")
 print(calNDVI(0.3,0.6))
-#print("Testing calculateNDVI MAIN")
+print("Testing calculateNDVI MAIN")
 
-#siteData <- read.csv("GOES_Paper_Sites.csv",header=TRUE)
-#siteData <- siteData[9,c(1,2,3,6)]
+siteData <- read.csv("GOES_Paper_Sites.csv",header=TRUE)
+siteData <- siteData[9,c(1,2,3,6)]
 
-#calculateNDVI_GOES_MAIN(day=251,siteData=siteData,TZ=5,dataPath="GOES_Data2017")
+calculateNDVI_GOES_MAIN(day=251,siteData=siteData,TZ=5,dataPath="GOES_Data2017")
