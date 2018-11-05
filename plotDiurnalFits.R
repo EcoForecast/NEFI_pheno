9959c2a44bac2fa09a341f9a05d839b11360f80a
Kathryn Wheeler <kiwheel@bu.edu>
2018-11-05 11:48:21 -0500

    changed to plot diurnal fits for all sites

--- plotDiurnalFits.R
+++ plotDiurnalFits.R
@@ -6,7 +6,6 @@
 library("ecoforecastR")
 library("rjags")
 
-
 diurnalExp <- function(a,c,k,xseq){
   k <- round(k,digits=1)
   #print(k)
@@ -19,45 +18,53 @@ diurnalExp <- function(a,c,k,xseq){
   return(c(left,right))
 }
 
-siteName <- "russellSage"
-diurnalFiles <- dir(path="dailyNDVI_GOES",pattern="varBurn2.RData")
-xseq <- seq(0,25,0.1)
-#i=1
-outputFileName <- paste(siteName,"_DiurnalFits_a.pdf",sep="")
+outputFileName <- paste(siteName,"ALL_DiurnalFits.pdf",sep="")
 pdf(file=outputFileName,width=45,height=40)
 par(mfrow=c(5,5))
-for(i in 1:length(diurnalFiles)){
-  load(paste("dailyNDVI_GOES/",diurnalFiles[i],sep=""))
-  dy <- substr(diurnalFiles[i],13,15)
-  print(diurnalFiles[i])
-  if(as.numeric(dy)<182){
-    yr <- "2018"
-  }
-  else{
-    yr <- "2017"
-  }
-  dat <- read.csv(paste("dailyNDVI_GOES/GOES_Diurnal_",siteName,"_",yr,dy,".csv",sep=""),header=FALSE)
-  if(typeof(var.burn)==typeof(FALSE)){
-    print(Paste(diurnalFiles[i], " did not converge",sep=""))
-    plot(as.numeric(dat[3,]),as.numeric(dat[2,]),main=paste("Didn't",diurnalFiles[i],sep=" "),xlab="Time",ylab="NDVI",ylim=c(0,1),xlim=c(0,25))
-  }
-  else{
-  out.mat <- as.matrix(var.burn)
-  a <- out.mat[,1]
-  c <- out.mat[,2]
-  k <- out.mat[,3]
-  ycred <- matrix(0,nrow=10000,ncol=length(xseq))
-  for(g in 1:10000){
-    Ey <- diurnalExp(a=a[g],c=c[g],k=k[g],xseq=xseq)
-    ycred[g,] <- Ey
-  }
-  ci <- apply(ycred,2,quantile,c(0.025,0.5, 0.975), na.rm= TRUE)
-  #plot(x=list(),y=list(),main=diurnalFiles[i],xlab="Time",ylab="NDVI",ylim=c(0,1),xlim=c(0,25))
-  plot(as.numeric(dat[3,]),as.numeric(dat[2,]),main=diurnalFiles[i],ylim=c(0,1),xlim=c(0,25))
-  ciEnvelope(xseq,ci[1,],ci[3,],col="lightBlue")
-  lines(xseq,ci[2,],col="black")
-  points(as.numeric(dat[3,]),as.numeric(dat[2,]))
-  abline(v=12,col="red")
+xseq <- seq(0,25,0.1)
+siteData <- read.csv("GOES_Paper_Sites.csv",header=TRUE)
+for(s in 1:nrow(siteData)){
+  
+  siteName <- as.character(siteData[s,1])
+  diurnalFiles <- intersect(dir(path="dailyNDVI_GOES",pattern="varBurn2.RData"),dir(path="dailyNDVI_GOES",pattern=siteName))
+  
+  for(i in 1:length(diurnalFiles)){
+    load(paste("dailyNDVI_GOES/",diurnalFiles[i],sep=""))
+    dy <- strsplit(diurnalFiles[i],"_")[[1]][2]
+    print(diurnalFiles[i])
+    print(dy)
+    print(as.numeric(dy))
+    if(as.numeric(dy)<182){
+      yr <- "2018"
+    }
+    else{
+      yr <- "2017"
+    }
+    dat <- read.csv(paste("dailyNDVI_GOES/GOES_Diurnal_",siteName,"_",yr,dy,".csv",sep=""),header=FALSE)
+    if(typeof(var.burn)==typeof(FALSE)){
+      print(paste(diurnalFiles[i], " did not converge",sep=""))
+      plot(as.numeric(dat[3,]),as.numeric(dat[2,]),main=paste("Didn't",diurnalFiles[i],sep=" "),xlab="Time",ylab="NDVI",ylim=c(0,1),xlim=c(0,25))
+    }
+    else{
+      out.mat <- as.matrix(var.burn)
+      a <- out.mat[,1]
+      rndNums <- sample(1:length(a),10000,replace=T)
+      a <- a[rndNums]
+      c <- out.mat[rndNums,2]
+      k <- out.mat[rndNums,3]
+      ycred <- matrix(0,nrow=10000,ncol=length(xseq))
+      for(g in 1:10000){
+        Ey <- diurnalExp(a=a[g],c=c[g],k=k[g],xseq=xseq)
+        ycred[g,] <- Ey
+      }
+      ci <- apply(ycred,2,quantile,c(0.025,0.5, 0.975), na.rm= TRUE)
+      #plot(x=list(),y=list(),main=diurnalFiles[i],xlab="Time",ylab="NDVI",ylim=c(0,1),xlim=c(0,25))
+      plot(as.numeric(dat[3,]),as.numeric(dat[2,]),main=diurnalFiles[i],ylim=c(0,1),xlim=c(0,25))
+      ciEnvelope(xseq,ci[1,],ci[3,],col="lightBlue")
+      lines(xseq,ci[2,],col="black")
+      points(as.numeric(dat[3,]),as.numeric(dat[2,]))
+      abline(v=12,col="red")
+    }
   }
 }
 dev.off()
