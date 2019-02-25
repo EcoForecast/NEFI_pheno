0d5f248795bb25ff1dff89e307dc25457f7059ad
Kathryn Wheeler <kiwheel@bu.edu>
2019-02-25 13:35:23 -0500

    removed b0 from the other process model loop

--- PhenoForecast/R/logisticCovPhenoModel.R
+++ PhenoForecast/R/logisticCovPhenoModel.R
@@ -37,7 +37,7 @@ logisticCovPhenoModel <- function(data,nchain){
   #### Process Model
   for(yr in 1:(N-1)){
     for(i in 2:n){
-      r[i,yr] <- b0 + b1 * Sf[i,yr]
+      r[i,yr] <- b1 * Sf[i,yr]
       color[i,yr] <- x[(i-1),yr] + r[i,yr] * x[(i-1),yr] * (1-x[(i-1),yr])  ## latent process
       Sf[i,yr] ~ dnorm(Sfmu[i,yr],Sfprec[i,yr])
       xl[i,yr] ~ dnorm(color[i,yr],p.proc)  ## process error
