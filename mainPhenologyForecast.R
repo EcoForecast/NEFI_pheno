0cde99f2a32f57d09a5a81c13132a85886e9944b
Kathryn Wheeler <kiwheel@bu.edu>
2019-02-28 14:05:23 -0500

    debugging

--- mainPhenologyForecast.R
+++ mainPhenologyForecast.R
@@ -43,7 +43,7 @@ if(length(files)==0){
   newDQFFileName <- paste(dataDirectory,siteName,"_","rel","_MOD13Q1_",startDate,"_",endDate,".csv",sep="") #File name for new DQF data downloaded
   if(!file.exists(newDQFFileName)){
     print("Downloading MODIS DQF File because no files are present")
-    try(mt_subset(product = "MOD13Q1",lat=lat,lon=long,band="250m_16_days_pixel_reliability",start=startDate,end=endDate,site_name = paste(siteName,"_rel",sep=""),out_dir = directory,internal=FALSE),silent=TRUE)
+    mt_subset(product = "MOD13Q1",lat=lat,lon=long,band="250m_16_days_pixel_reliability",start=startDate,end=endDate,site_name = paste(siteName,"_rel",sep=""),out_dir = directory,internal=FALSE)
   }
 }
 downloadMODIS(startDate=startDate,endDate=endDate,metric="NDVI",dataDirectory=dataDirectory,lat=lat,long=long,siteName=siteName)
