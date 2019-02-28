9571205db93c82327b0be20405ff1a115ce17e5c
Kathryn Wheeler <kiwheel@bu.edu>
2019-02-28 14:16:19 -0500

    corrected the directory name somewhere else

--- mainPhenologyForecast.R
+++ mainPhenologyForecast.R
@@ -40,11 +40,10 @@ startDate <- as.Date(siteData[i,7])
 files <- intersect(dir(path=dataDirectory,pattern=paste(siteName,"_rel",sep="")),dir(path=dataDirectory,pattern="MOD13Q1")) #Current downloaded data files
 
 if(length(files)==0){
-  directory=paste(getwd(),"/",dataDirectory,sep="")
   newDQFFileName <- paste(dataDirectory,siteName,"_","rel","_MOD13Q1_",startDate,"_",endDate,".csv",sep="") #File name for new DQF data downloaded
   if(!file.exists(newDQFFileName)){
     print("Downloading MODIS DQF File because no files are present")
-    mt_subset(product = "MOD13Q1",lat=lat,lon=long,band="250m_16_days_pixel_reliability",start=startDate,end=endDate,site_name = paste(siteName,"_rel",sep=""),out_dir = directory,internal=FALSE)
+    mt_subset(product = "MOD13Q1",lat=lat,lon=long,band="250m_16_days_pixel_reliability",start=startDate,end=endDate,site_name = paste(siteName,"_rel",sep=""),out_dir = dataDirectory,internal=FALSE)
   }
 }
 downloadMODIS(startDate=startDate,endDate=endDate,metric="NDVI",dataDirectory=dataDirectory,lat=lat,long=long,siteName=siteName)
